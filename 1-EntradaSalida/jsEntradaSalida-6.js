ad7f6e5e532f8f3440b13db87549bc7476ad6661
sabrinallopart <40778233+sabrinallopart@users.noreply.github.com>
2018-07-05 21:57:44 -0300

    Add files via upload
    clase 2

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,20 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
+	var numeroUno;
+	var numeroDos;
+	var resultado; 
+
+	numeroUno = document.getElementById('numeroUno').value;
+	numeroUno = parseInt(numeroUno);
+
+	numeroDos = document.getElementById('numeroDos').value;
+	numeroDos = parseInt(numeroDos);
+
+	resultado = document.getElementById('resultado').value;
+	
+
+
 
 }
 
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -3,7 +3,19 @@ transformarlo a entero (parseInt), luego
 mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
-{
+{	
+	var importe;
+	var aumento; 
+	var resultado; 
+
+	importe = document.getElementById("sueldo").value;
+
+	parseInt("importe");
+
+	
+
+
+
 
 	
 }
