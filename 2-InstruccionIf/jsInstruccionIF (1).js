ad3c2db9dbd2a27911d935b00232822119ec402e
sabrinallopart <40778233+sabrinallopart@users.noreply.github.com>
2018-07-12 20:38:55 -0300

    clase nro 4

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -2,6 +2,21 @@ function mostrar()
 {
 //tomo la edad  
 
+	var campoEdad;
+
+	campoEdad=document.getElementById("edad").value;
+
+	if (campoEdad==15) {
+		
+
+	campoEdad=alert("niña bonita");}
+
+}
 
 
-}//FIN DE LA FUNCIÓN
+
+//FIN DE LA FUNCIÓN
+
+
+//if else
+
--- 2-InstruccionIf/jsInstruccionIF (2).js
+++ 2-InstruccionIf/jsInstruccionIF (2).js
@@ -2,6 +2,20 @@ function mostrar()
 {
 //tomo la edad  
 
+	var campoEdad;
 
+		campoEdad=document.getElementById("edad").value;
+
+		if (campoEdad<=18) {
+
+		campoEdad=alert("es mayor");}
+
+		if (campoEdad>/18) {} else {
+
+			campoEdad=alert("es mejor");}
+		}
+
+
+	
 
 }//FIN DE LA FUNCIÓN
